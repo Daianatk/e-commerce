23c3bde22440bb3f96bb303d6dc7d7b04891998b
Daianatk <daiana.campos04@gmail.com>
2019-09-12 23:13:22 -0500

    Updates

---
+++ precache-manifest.32f5322555fbfd93096bde0ee503ef3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e90cedd9b7ccb3f02b62",
+    "url": "/e-commerce/static/css/main.2f233935.chunk.css"
+  },
+  {
+    "revision": "e90cedd9b7ccb3f02b62",
+    "url": "/e-commerce/static/js/main.6d9a0be6.chunk.js"
+  },
+  {
+    "revision": "290256ac1007569aecdb",
+    "url": "/e-commerce/static/js/runtime~main.8575550b.js"
+  },
+  {
+    "revision": "33ecb4a1063073fd0afb",
+    "url": "/e-commerce/static/css/2.befa2672.chunk.css"
+  },
+  {
+    "revision": "33ecb4a1063073fd0afb",
+    "url": "/e-commerce/static/js/2.307c037a.chunk.js"
+  },
+  {
+    "revision": "6a3f2695f1260c4e71a263ccecd57e93",
+    "url": "/e-commerce/static/media/logo.6a3f2695.svg"
+  },
+  {
+    "revision": "31549ad9736f176aaa0d4129c863ad67",
+    "url": "/e-commerce/index.html"
+  }
+];
